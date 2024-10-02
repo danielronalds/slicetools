059c0d7d796a9e82eb9810dfc4971c46390fe65b
Daniel R <nzdanron@gmail.com>
2024-10-02 21:49:08 +1300

    feat: add Tail, Head, and Foldl Functions

--- slicetools.go
+++ slicetools.go
@@ -23,3 +23,38 @@ func Filter[X any](xs []X, pred func(X) bool) []X {
 
 	return filteredXs
 }
+
+// Returns a slice without it's first element
+func Tail[X any](xs []X) []X {
+	if len(xs) == 0 {
+		return xs
+	}
+	return xs[1:]
+}
+
+// Returns the first element of the array
+func Head[X any](xs []X) X {
+	if len(xs) == 0 {
+		var def X
+		return def
+	}
+	return xs[0]
+}
+
+// Applies a function, fn, to a slice, reducing it to a value of type Y. Applies from the first
+// element first to the last (left to right)
+func Foldl[X, Y any](xs []X, init Y, fn func(Y, X) Y) Y {
+	currXs := xs
+	currVal := init
+
+	for true {
+		if len(currXs) == 0 {
+			break
+		}
+		x := Head(currXs)
+		currVal = fn(currVal, x)
+		currXs = Tail(currXs)
+	}
+
+	return currVal
+}
--- slicetools_test.go
+++ slicetools_test.go
@@ -88,3 +88,117 @@ func TestFilterEmpty(t *testing.T) {
 		t.Fatalf("Function was not applied as expected")
 	}
 }
+
+func TestTail(t *testing.T) {
+	xs := []int{1, 2, 3, 4, 5}
+
+	expected := []int{2, 3, 4, 5}
+
+	if slices.Compare(Tail(xs), expected) != 0 {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestTailOneElement(t *testing.T) {
+	xs := []int{1}
+
+	expected := make([]int, 0)
+
+	if slices.Compare(Tail(xs), expected) != 0 {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestTailEmpty(t *testing.T) {
+	xs := make([]int, 0)
+
+	expected := make([]int, 0)
+
+	if slices.Compare(Tail(xs), expected) != 0 {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestHead(t *testing.T) {
+	xs := []int{1, 2, 3, 4, 5}
+
+	expected := 1
+
+	if Head(xs) != expected {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestHeadEmpty(t *testing.T) {
+	xs := make([]int, 0)
+
+	expected := 0 // The default value for an int
+
+	if Head(xs) != expected {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestFoldl(t *testing.T) {
+	xs := []int{1, 2, 3, 4, 5}
+
+	fn := func(total, val int) int {
+		return total + val
+	}
+
+	expected := 15
+
+	actual := Foldl(xs, 0, fn)
+
+	if actual != expected {
+		t.Fatalf("Function was not applied as expected, wanted %v got %v", expected, actual)
+	}
+}
+
+func TestFoldlComplex(t *testing.T) {
+	xs := []int{-20, 5, 2}
+
+	fn := func(total, val int) int {
+		return total - val
+	}
+
+	expected := 13
+
+	actual := Foldl(xs, 0, fn)
+
+	if actual != expected {
+		t.Fatalf("Function was not applied as expected, wanted %v got %v", expected, actual)
+	}
+}
+
+func TestFoldlOneElement(t *testing.T) {
+	xs := []int{2}
+
+	fn := func(total, val int) int {
+		return total + (val * 2)
+	}
+
+	expected := 4
+
+	actual := Foldl(xs, 0, fn)
+
+	if actual != expected {
+		t.Fatalf("Function was not applied as expected, wanted %v got %v", expected, actual)
+	}
+}
+
+func TestFoldlEmpty(t *testing.T) {
+	xs := make([]int, 0)
+
+	fn := func(total, val int) int {
+		return total + (val * 2)
+	}
+
+	expected := 10 // Should be the init value
+
+	actual := Foldl(xs, 10, fn)
+
+	if actual != expected {
+		t.Fatalf("Function was not applied as expected, wanted %v got %v", expected, actual)
+	}
+}
