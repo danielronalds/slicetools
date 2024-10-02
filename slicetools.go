7bacf8e7ce03feba0acb92a49a5cf87632a6145c
Daniel R <nzdanron@gmail.com>
2024-10-02 20:49:22 +1300

    feat: add Filter function with tests

--- slicetools.go
+++ slicetools.go
@@ -10,3 +10,16 @@ func Map[X, Y any](xs []X, fn func (X) Y) []Y {
 
 	return yList
 }
+
+// Iterates over xs returning a slice containing only elements of xs where the predicate was true
+func Filter[X any](xs []X, pred func (X) bool) []X {
+	filteredXs := make([]X, 0)
+
+	for _, x := range xs {
+		if pred(x) {
+			filteredXs = append(filteredXs, x)
+		}
+	}
+
+	return filteredXs
+}
--- slicetools_test.go
+++ slicetools_test.go
@@ -46,3 +46,45 @@ func TestMapEmpty(t *testing.T) {
 		t.Fatalf("Function was not applied as expected")
 	}
 }
+
+func TestFilter(t *testing.T) {
+	xs := []int {1,2,3,4,5} 
+
+	isEven := func(x int) bool {
+		return (x % 2) == 0
+	}
+
+	expected := []int{2,4}
+
+	if slices.Compare(Filter(xs, isEven), expected) != 0 {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestFilterOneElement(t *testing.T) {
+	xs := []int {1} 
+
+	isEven := func(x int) bool {
+		return (x % 2) == 0
+	}
+
+	expected := make([]int, 0)
+
+	if slices.Compare(Filter(xs, isEven), expected) != 0 {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
+
+func TestFilterEmpty(t *testing.T) {
+	xs := make([]int, 0) 
+
+	isEven := func(x int) bool {
+		return (x % 2) == 0
+	}
+
+	expected := make([]int, 0)
+
+	if slices.Compare(Filter(xs, isEven), expected) != 0 {
+		t.Fatalf("Function was not applied as expected")
+	}
+}
